0dbdfe919074ff4d85ee51ccdb266f8243426241
unknown <garroashly7@gmail.com>
2026-03-26 19:06:30 -0600

    feat: modificar limpieza para lectura de csv

--- codigo/01_limpieza.R
+++ codigo/01_limpieza.R
@@ -1,4 +1,9 @@
 # 01_limpieza.R
 # Limpieza y preparación de los datos crudos
-# Autor:
-# Fecha:
+# Autor: Alessandro Umaña, Emily Sanchez, Debbie Con, Ashly Garro
+# Fecha: 26-03-2026
+
+library(tidyverse)
+
+informacion_accidentes <- read_csv("C:/Users/HP/OneDrive/Escritorio/Accident_Information.csv")
+View(informacion_accidentes)
