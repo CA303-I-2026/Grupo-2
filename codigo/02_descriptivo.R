081dfd6d5b67a73a936226bdbc461dbc1e32c0d3
Emily Sanchez <emily.sanchezmancia@ucr.ac.cr>
2026-04-24 12:50:54 -0600

    feat(descriptivo): Se agregan 3 gráficos y 1 cuadro al análisis descriptivo

--- codigo/02_descriptivo.R
+++ codigo/02_descriptivo.R
@@ -69,3 +69,54 @@ ggplot(Accidentes, aes(x = Weather_Conditions, fill = Weather_Conditions)) +
   ) +
   coord_flip() +
   theme(legend.position = "none")
+
+#-----------------------------------------------------------------------------
+# Gráfico de barras: Cantidad de accidentes según Road_Type y Light_Conditions
+#-----------------------------------------------------------------------------
+
+ggplot(Accidentes, aes(x = Road_Type, fill = Light_Conditions)) +
+  geom_bar(position = "dodge") +
+  scale_fill_manual(values = paleta) +
+  labs(
+    title = "Cantidad de accidentes según tipo de calle y condiciones de luz",
+    x = "Tipo de calle",
+    y = "Cantidad de accidentes"
+  ) +
+  coord_flip()
+
+#-------------------------------------------------------------------------
+# Cuadro: Cantidad de accidentes según Urban_or_Rural_Area 
+#-------------------------------------------------------------------------
+
+cuadro_zona <- Accidentes %>%
+  count(Urban_or_Rural_Area, name = "Cantidad de accidentes") %>%
+  arrange(desc("Cantidad de accidentes"))
+
+cuadro_zona
+
+#-------------------------------------------------------------------------
+# Gráfico de barras: Conteo de accidentes por Accident_Severity
+#-------------------------------------------------------------------------
+
+ggplot(Accidentes, aes(x = Accident_Severity, fill = Accident_Severity)) +
+  geom_bar() +
+  scale_fill_manual(values = paleta) +
+  labs(
+    title = "Cantidad de accidentes por severidad",
+    x = "Severidad",
+    y = "Frecuencia"
+  ) +
+  theme(legend.position = "none")
+
+#----------------------------------------------------------------------------------
+# Gráfico de barras: Conteo de accidentes por Accident_Severity agrupado por Year
+#----------------------------------------------------------------------------------
+
+ggplot(Accidentes, aes(x = factor(Year), fill = Accident_Severity)) +
+  geom_bar(position = "dodge") +
+  scale_fill_manual(values = paleta) +
+  labs(
+    title = "Conteo de accidentes por severidad y año (2010–2017)",
+    x = "Año",
+    y = "Cantidad de accidentes"
+  )
